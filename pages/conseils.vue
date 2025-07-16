0ea0fe9b6e490eb895dd8e74963203e8cbcbd902
ario994 <aristide.adouko@epitech.eu>
2025-07-16 12:27:25 +0000

    try: test workflow CI/CD

--- pages/conseils.vue
+++ pages/conseils.vue
@@ -46,7 +46,7 @@
       </div>
     </section>
 
-    <!-- Featured Article -->
+    <!-- Featured Articles -->
     <section class="section-padding bg-gray-50">
       <div class="container-custom">
         <div class="card overflow-hidden">
